4233ec481e770389d2c49730ffb97dc7dbcfa85a
= <stonealex145@gmail.com>
2022-10-15 10:46:18 -0400

    Pretty much done

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,41 @@
 mod shape;
 mod tensor;
 
-pub use shape::Shape;
-pub use tensor::*;
-
-
 pub fn test() {
     let t1 = tensor::full(&[3, 3, 3], 'h');
     let t3 = tensor::range(&[3, 3, 3]);
     let t2 = t3.get_sub(&[2]).unwrap();
+    let t4 = tensor::from_flat_vec(&[2, 2], vec![1, 2, 3, 3]).unwrap();
 
     println!("{:?}", t3);
     println!("{:?}", t2);
+    println!("{:?}", t1);
+    println!("{:?}", t4);
+    println!("{:?}", t4.map(|x| x*2));
 
+    let a = tensor::range(&[3, 2]);
+    let b = tensor::range(&[2, 3]).map(|x| x * 2);
+    let c = tensor::mul(&a, &b).unwrap();
+
+    println!("{:?}", a);
+    println!("{:?}", b);
+    println!("{:?}", c);
+    println!("{:?}", tensor::sum(&tensor::range(&[2, 2])));
+
+
+    let identity = tensor::identity2(3);
+    let mult = tensor::from_flat_vec(&[3, 1], vec![
+                                     1.0,
+                                     2.0,
+                                     3.0,
+    ]).unwrap();
+    let output = tensor::matmul(&identity, &mult).unwrap();
+    println!("{:?}", identity);
+    println!("{:?}", mult);
+    println!("{:?}", output);
 }
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/tensor.rs
+++ src/tensor.rs
@@ -1,6 +1,6 @@
 use crate::shape::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Tensor<T: Clone> {
     shape: Vec<usize>,
     elements: Vec<T>
@@ -58,26 +58,127 @@ impl<T: Clone> Tensor<T> {
             elements: self.elements.clone()
         })
     }
-}
-pub fn full<T: Clone>(shape: Shape, value: T) -> Tensor<T> {
-    let length = compute_size_of_shape(shape);
-    let mut elements = Vec::<T>::with_capacity(length);
-    // crudy memcpy
-    for _ in 0..length {elements.push(value.clone());}
-    //elements.fill(value);
-    Tensor {
-        shape: shape.iter().map(|x| *x).collect(),
-        elements
-    }
-}
-pub fn range(shape: Shape) -> Tensor<i32> {
-    let length = compute_size_of_shape(shape);
-    let elements = (0..length).map(|x| x as i32).collect();
-    Tensor {
-        shape: shape.iter().map(|x| *x).collect(),
-        elements
+    pub fn map(&self, rule: fn(T) -> T) -> Self {
+        let mut new_tensor = self.clone();
+        for i in 0..new_tensor.elements.len() {
+            new_tensor.elements[i] = rule(new_tensor.elements[i].clone());
+        }
+        new_tensor
     }
 }
 
+pub use create::*;
+pub use ops::*;
+
+mod ops {
+    use super::*;
+    pub fn individual_binary_operation<T: Clone>(a: &Tensor<T>, b: &Tensor<T>, rule: fn(T, T) -> T)
+        -> Result<Tensor<T>, &'static str> {
+        if a.size() != b.size() {
+            return Err("Size of a and b tensors must be the same");
+        }
+        let mut new_tensor = a.clone();
+        for i in 0..new_tensor.elements.len() {
+            new_tensor.elements[i] = rule(new_tensor.elements[i].clone(), b.elements[i].clone());
+        }
+        Ok(new_tensor)
+    }
+    pub fn add<T: Clone + std::ops::Add<Output=T>>(a: &Tensor<T>, b: &Tensor<T>)
+        -> Result<Tensor<T>, &'static str> {
+        Ok(individual_binary_operation(a, b, |x, y| x + y)?)
+    }
+    pub fn sub<T: Clone + std::ops::Sub<Output=T>>(a: &Tensor<T>, b: &Tensor<T>)
+        -> Result<Tensor<T>, &'static str> {
+        Ok(individual_binary_operation(a, b, |x, y| x - y)?)
+    }
+    pub fn mul<T: Clone + std::ops::Mul<Output=T>>(a: &Tensor<T>, b: &Tensor<T>)
+        -> Result<Tensor<T>, &'static str> {
+        Ok(individual_binary_operation(a, b, |x, y| x * y)?)
+    }
+    pub fn div<T: Clone + std::ops::Div<Output=T>>(a: &Tensor<T>, b: &Tensor<T>)
+        -> Result<Tensor<T>, &'static str> {
+        Ok(individual_binary_operation(a, b, |x, y| x / y)?)
+    }
+    pub fn sum<T: Clone + std::ops::Add<Output=T> + Default>(summed: &Tensor<T>) -> T {
+        let mut summation = T::default(); // default used here
+        for element in summed.elements.iter() {
+            summation = summation + element.clone();
+        }
+        summation
+    }
+    pub fn dot<T: Clone + std::ops::Mul<Output=T> + std::ops::Add<Output=T> + Default>(a: &Tensor<T>,
+                                                   b: &Tensor<T>) -> Result<T, &'static str> {
+        Ok(sum(&(mul(a, b)?)))
+    }
+    pub fn matmul<T>(a: &Tensor<T>, b: &Tensor<T>) -> Result<Tensor<T>, &'static str>
+        where T: Clone + Default + std::ops::Mul<Output=T>
+        + std::ops::AddAssign,
+    {
+        if a.shape.len() != 2 && b.shape.len() != 2 {
+            return Err("Cannot multiply matrices with dimensionality != 2");
+        }
+        if a.shape[1] != b.shape[0] {
+            return Err("Matrix a columns must match matrix b rows");
+        }
+        /*
+         * rows by columns
+         * first row then column
+         * a: 3*2 matrix
+         * b: 2*3 matrix
+         * c = 3 * 3
+         */
+        let mut new_tensor = full(&[a.shape[0], b.shape[1]], T::default());
+
+        for i in 0..a.shape[0] {
+            for j in 0..b.shape[1] {
+                let mut value = T::default();
+                for k in 0..b.shape[0] {
+                    value += a.get_value(&[i, k]).unwrap() * b.get_value(&[k, j]).unwrap();
+                }
+                new_tensor.set_value(&[i, j], value).expect("Something went terribly wrong");
+            }
+        }
+        Ok(new_tensor)
+    }
+}
+
+mod create {
+    use super::*;
+    pub fn full<T: Clone>(shape: Shape, value: T) -> Tensor<T> {
+        let length = compute_size_of_shape(shape);
+        let mut elements = Vec::<T>::with_capacity(length);
+        // crudy memcpy
+        for _ in 0..length {elements.push(value.clone());}
+        //elements.fill(value);
+        Tensor {
+            shape: shape.iter().map(|x| *x).collect(),
+            elements
+        }
+    }
+    pub fn range(shape: Shape) -> Tensor<i32> {
+        let length = compute_size_of_shape(shape);
+        let elements = (0..length).map(|x| x as i32).collect();
+        Tensor {
+            shape: shape.iter().map(|x| *x).collect(),
+            elements
+        }
+    }
+    pub fn from_flat_vec<T: Clone>(shape: Shape, elements: Vec<T>) -> Result<Tensor<T>, &'static str> {
+        if elements.len() != compute_size_of_shape(shape) {
+            return Err("Element count does not match size of shape")
+        }
+        Ok(Tensor {
+            shape: shape.iter().map(|x| *x).collect(),
+            elements
+        })
+    }
+    pub fn identity2(count: usize) -> Tensor<f64> {
+        let mut new_tensor = full(&[count, count], 0.0);
+        for i in 0..count {
+            new_tensor.set_value(&[i, i], 1.0).expect("Something went terribly wrong");
+        }
+        new_tensor
+    }
+}
 
 
